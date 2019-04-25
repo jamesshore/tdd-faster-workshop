168790e06ea535f3ef9a93e5affc5d5b4b68c563
James Shore <jshore@jamesshore.com>
2019-04-25 11:24:47 -0700

    Step 3 complete: integrate logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
+const rot13 = require("./rot13.js");
+
 module.exports = class App {
 
 	run(input) {
-		return "uryyb";
+		return rot13.transform(input);
 	}
 
 };
