04741125ce9248859461d36f672a2d08d84409a8
James Shore <jshore@jamesshore.com>
2019-04-25 11:07:47 -0700

    Step 1 complete: trivial application layer

--- src/_app_test.js
+++ src/_app_test.js
@@ -2,11 +2,14 @@
 "use strict";
 
 const assert = require("./assert.js");
+const App = require("./app.js");
 
-describe("Nothing", function() {
+describe("Application", function() {
 
-	it("runs tests", function() {
-		assert.todo();
+	it("ROT-13s input", function() {
+		const app = new App();
+		const response = app.run("hello");
+		assert.equal(response, "uryyb");
 	});
 
 });
---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+module.exports = class App {
+
+	run(input) {
+		return "uryyb";
+	}
+
+};
