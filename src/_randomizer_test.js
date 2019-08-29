0598a4199cd1645fcc4314559c2936d680ce8c15
James Shore <jshore@jamesshore.com>
2019-08-28 18:05:17 -0700

    Add Randomizer.createNull()

--- src/_randomizer_test.js
+++ src/_randomizer_test.js
@@ -22,4 +22,41 @@ describe("Random Number", function() {
 		}
 	});
 
+
+	describe("Null", function() {
+
+		it("provides hard-coded values", function() {
+			const randomizer = Randomizer.createNull([ 0.1, 0.9, 0.42 ]);
+			assert.deepEqual(randomizer.generate(3), [ 0.1, 0.9, 0.42 ]);
+		});
+
+		it("provides internal default if no values provided at all", function() {
+			const randomizer = Randomizer.createNull();
+			assert.deepEqual(randomizer.generate(3), [ 0.99, 0.99, 0.99 ]);
+		});
+
+		it("fails fast when no more values are available", function() {
+			const randomizer = Randomizer.createNull([]);
+			assert.exception(
+				() => randomizer.generate(1),
+				"No more values available in null Randomizer"
+			);
+		});
+
+		it("fails fast when hard-coded values are outside legal range", function() {
+			let expectedMessage = " is outside legal range for random numbers (should be between 0 and 1, excluding 1)";
+			assert.exception(
+				() => Randomizer.createNull([ 0.1, 1 ]),
+				`'1'${expectedMessage}`,
+				"too large"
+			);
+			assert.exception(
+				() => Randomizer.createNull([ 0, -1 ]),
+				`'-1'${expectedMessage}`,
+				"too small"
+			);
+		});
+
+	});
+
 });
--- src/randomizer.js
+++ src/randomizer.js
@@ -1,18 +1,53 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
+const NULL_DEFAULT = 0.99;
+
 module.exports = class Randomizer {
 
 	static create() {
-		return new Randomizer();
+		return new Randomizer(Math);
+	}
+
+	static createNull(values) {
+		return new Randomizer(new NullMath(values));
+	}
+
+	constructor(math) {
+		this._math = math;
 	}
 
 	generate(amount) {
 		const result = [];
 		for (let i = 0; i < amount; i++) {
-			result.push(Math.random());
+			result.push(this._math.random());
 		}
 		return result;
 	}
 
 };
+
+
+class NullMath {
+
+	constructor(values) {
+		if (values !== undefined) failIfValuesOutOfRange(values);
+
+		this._values = values;
+	}
+
+	random() {
+		if (this._values === undefined) return NULL_DEFAULT;
+		if (this._values.length !== 0) return this._values.shift();
+		throw new Error("No more values available in null Randomizer");
+	}
+
+}
+
+function failIfValuesOutOfRange(values) {
+	values.forEach((val) => {
+		if (val < 0 || val >= 1) {
+			throw new Error(`'${val}' is outside legal range for random numbers (should be between 0 and 1, excluding 1)`);
+		}
+	});
+}
