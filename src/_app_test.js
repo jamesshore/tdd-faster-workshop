5e933cf5ba2719aaf89f910282a65451689e6e27
James Shore <jshore@jamesshore.com>
2019-08-28 19:17:11 -0700

    Integrate randomizer

--- src/_app_test.js
+++ src/_app_test.js
@@ -4,13 +4,15 @@
 const assert = require("./assert.js");
 const App = require("./app.js");
 const CommandLine = require("./command_line.js");
+const Randomizer = require("./randomizer.js");
 
 describe("Application", function() {
 
 	it("rolls dice", function() {
 		const cli = CommandLine.createNull("4d12");
+		const randomizer = Randomizer.createNull([ 0, 0, 0, 0 ]);
 
-		const app = new App(cli);
+		const app = new App(cli, randomizer);
 		app.run();
 		assert.equal(cli.getLastOutput(), 4);
 	});
--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@
 
 const CommandLine = require("./command_line");
 const DiceExpression = require("./dice_expression");
+const Randomizer = require("./randomizer.js");
 
 module.exports = class App {
 
-	constructor(cli = CommandLine.create()) {
+	constructor(cli = CommandLine.create(), randomizer = Randomizer.create()) {
 		this._cli = cli;
+		this._randomizer = randomizer;
 	}
 
 	run() {
 		const input = this._cli.arg();
 		const expression = DiceExpression.create(input);
-		const output = expression.value([ 0, 0, 0, 0 ]);
+
+		const randomNumbers = this._randomizer.generate(expression.numDice());
+		const output = expression.value(randomNumbers);
+
 		this._cli.output(output);
 	}
 
